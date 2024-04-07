0c9a0856f50fe35484a13a12f24f3b916abdd960
kawsar ahmed <kawsar.qtec@gmail.com>
2024-04-08 05:39:01 +0600

    added footer

---
+++ assets/js/tooltip/tooltip.js
@@ -0,0 +1,20 @@
+const tooltipTriggerList = [].slice.call(
+  document.querySelectorAll('[data-bs-toggle="tooltip"]')
+);
+const tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl);
+});
+console.log('ok');
+const fetchData = async (url) => {
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Error:', error);
+        throw error;
+    }
+};
