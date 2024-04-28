a91e2745403e47d79dab8d29729bf18548f37713
kawsar ahmed <kawsar.qtec@gmail.com>
2024-04-28 17:46:23 +0600

    fixed all page preloader

---
+++ assets/js/loader/loader.js
@@ -0,0 +1,10 @@
+const preloader = document.querySelector('#preloader');
+
+function showHidePreloader() {
+  preloader.classList.remove('d-none');
+  setTimeout(() => {
+    preloader.classList.add('d-none');
+  }, 2000); // 5000 milliseconds = 5 seconds
+}
+
+window.addEventListener('load', showHidePreloader);
